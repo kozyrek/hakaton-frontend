35023530d10019b5b33f1c031d6a3d9a22f1abae
unknown <ratio123@yandex.ru>
2025-06-11 16:25:20 +0300

    add pagination

--- src/api/getAllUsers.js
+++ src/api/getAllUsers.js
@@ -1,6 +1,6 @@
 import { HTTP } from "./http";
 
-export default async function getAllUser(params, page = 1) {
+export default async function getAllUser(params, page = 1, perPage = 10) {
   const queryString = new URLSearchParams();
 
   if (params?.search) {
@@ -15,6 +15,9 @@ export default async function getAllUser(params, page = 1) {
   if (params?.is_mentor !== null && params?.is_mentor !== undefined) {
     queryString.append("is_mentor", params.is_mentor);
   }
+
+  queryString.append("page", page.toString());
+  queryString.append("per_page", perPage.toString());
 
   const response = await HTTP.get(`/users?${queryString.toString()}`);
 
