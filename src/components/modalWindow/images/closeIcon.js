9ae158433cf96194bedce4cf8ffbaa7d19cc874e
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2025-09-19 01:00:58 +0500

    fix ошибки 422 регистрации

--- src/components/modalWindow/images/closeIcon.js
+++ src/components/modalWindow/images/closeIcon.js
@@ -10,14 +10,14 @@ export default function CloseIcon() {
       <path
         d="M17.5 2.5L2.5 17.5"
         stroke="#1A1F1F"
-        stroke-width="2"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M2.5 2.5L17.5 17.5"
         stroke="#1A1F1F"
-        stroke-width="2"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
